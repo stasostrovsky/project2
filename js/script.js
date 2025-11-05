00108a9bc70c25f7fc9e9384d56a34e34a12eefe
Stas <stas_ostrovsky@hotmail.com>
2025-11-05 17:39:05 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let num1 = 10,
+  num2 = 20,
+  result;
+
+if (result === null || result === undefined) {
+  if (num1 !== null && num1 !== undefined) {
+    result = num1;
+  } else {
+    result = num2;
+  }
+}
+
+result = num1 ?? num2;
