28028fa3f0d8b816a8f2469407ce8c8d66b35462
Stas <stas_ostrovsky@hotmail.com>
2025-11-07 15:44:53 +0200

    test3

--- js/script.js
+++ js/script.js
@@ -1,65 +1,36 @@
-/* Задание на урок:
+// Место для первой задачи
+function sayHello(userName) {
+  userName = "Привет, " + userName + "!";
+  //userName = `Привет ${userName} !`;
 
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
-str.length - и получить её длину)
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
-"use strict";
-
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
-
-/* for (let i = 0; i < 2; i++) {
-  const a = prompt("Один из последних просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его?", "");
-
-  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-    personalMovieDB.movies[a] = b;
-    console.log("done");
-  } else {
-    console.log("error");
-    i--;
-  }
-} */
-
-/* if (personalMovieDB.count < 10) {
-  console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-  console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-  console.log("Вы киноман");
-} else {
-  console.log("Произошла ошибка");
-} */
-
-switch (true) {
-  case personalMovieDB.count >= 10 && personalMovieDB.count < 30:
-    console.log("Вы классический зритель");
-    break;
-  case personalMovieDB.count < 10 && personalMovieDB.count > 0:
-    console.log("Просмотрено довольно мало фильмов");
-    break;
-  case personalMovieDB.count >= 30:
-    console.log("Вы киноман");
-    break;
-  default:
-    console.log("Произошла ошибка");
-    break;
+  return userName;
 }
-console.log(personalMovieDB);
+console.log(sayHello("stas"));
+
+// Место для второй задачи
+function returnNeighboringNumbers(myNumber) {
+  let arr = [];
+  --myNumber;
+  for (let i = 0; i < 3; i++) {
+    arr[i] = myNumber;
+    myNumber++;
+  }
+  return arr;
+}
+console.log(returnNeighboringNumbers(10));
+
+// Место для третьей задачи
+function getMathResult(Baza, Povtorit) {
+  if (String(typeof Povtorit) != "number" || Povtorit <= 0) {
+    return Baza;
+  }
+
+  let res = "";
+
+  for (let i = 1; i <= Povtorit; i++) {
+    res = res + Baza * i + (i < Povtorit ? "---" : "");
+  }
+
+  return res;
+}
+console.log(getMathResult(10, 5));
