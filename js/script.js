76db4c628c5a9182d50eb86e48551870024ee454
Stas <stas_ostrovsky@hotmail.com>
2025-11-11 10:37:50 +0200

    test

--- js/script.js
+++ js/script.js
@@ -79,3 +79,108 @@ function writeYourGenres() {
 }
 
 writeYourGenres();
+
+console.log(calculateVolumeAndArea("5"));
+
+function calculateVolumeAndArea(lengthsEdgeCube) {
+  const x = Number(lengthsEdgeCube);
+
+  if (!Number.isFinite(x) || x < 0 || x.toString().includes(".")) {
+    return "ввели хрень!";
+  }
+
+  let str = `Объем куба: ${x * x * x}, площадь всей поверхности: ${6 * x * x}`;
+  return str;
+}
+
+console.log(getCuopeNumber("10"));
+
+function getCuopeNumber(place) {
+  const x = Number(place);
+
+  if (!Number.isFinite(x) || x < 0 || x.toString().includes(".")) {
+    return "Ошибка. Проверьте правильность введенного номера места";
+  }
+
+  if (x === 0 || x > 36) {
+    return "Таких мест в вагоне не существует";
+  }
+
+  let minPlace = 1;
+  let maxPlace = 4;
+
+  for (let cuopeNumberindex = 1; cuopeNumberindex < 10; cuopeNumberindex++) {
+    if (place >= minPlace && place <= maxPlace) {
+      return cuopeNumberindex;
+    }
+    minPlace += 4;
+    maxPlace += 4;
+  }
+}
+
+console.log(getTimeFromMinutes(135));
+
+function getTimeFromMinutes(fromMinutes) {
+  let minutes = 0;
+  let hour = 0;
+  if (
+    typeof fromMinutes != "number" ||
+    !Number.isFinite(fromMinutes) ||
+    fromMinutes <= 0
+  ) {
+    return "хрень ввели!";
+  }
+
+  hour = Math.floor(fromMinutes / 60);
+  minutes = fromMinutes - hour * 60;
+
+  let nameHour = "";
+
+  if (hour === 1) {
+    nameHour = "час";
+  } else if (hour > 1 && hour < 5) {
+    nameHour = "часа";
+  } else {
+    nameHour = "часов";
+  }
+
+  let str = `Это ${hour} ${nameHour} и ${minutes} минут`;
+
+  return str;
+}
+//++++++++++++++++++++++++++++++++++++++
+console.log(fib(8));
+
+function fib(countIter) {
+  let num = "";
+
+  if (
+    typeof countIter !== "number" ||
+    countIter === 0 ||
+    !Number.isInteger(countIter)
+  ) {
+    return num;
+  }
+
+  if (countIter === 1) {
+    return "0";
+  }
+
+  let arr = [];
+  for (let i = 0; i < countIter; i++) {
+    if (i <= 1) {
+      arr[i] = i;
+    } else {
+      arr[i] = arr[i - 2] + arr[i - 1];
+    }
+  }
+
+  for (let i = 0; i < countIter; i++) {
+    num = num + arr[i] + ' ';
+  }
+
+  return num;
+}
+
+console.log((0.1 + 0.2).toFixed(2)==0.3);
+
