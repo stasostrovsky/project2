03506fcbd3b229865914b7911a286880ce2315f5
Stas <stas_ostrovsky@hotmail.com>
2025-11-14 11:37:13 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,23 +1,60 @@
-let users = [
-  { id: "john", name: "John Smith", age: 20 },
-  { id: "ann", name: "Ann Smith", age: 24 },
-  { id: "pete", name: "Pete Peterson", age: 31 },
-];
+const personalPlanPeter = {
+  name: "Peter",
+  age: "29",
+  skills: {
+    languages: ["ru", "eng"],
+    programmingLangs: {
+      js: "20%",
+      php: "10%",
+    },
+    exp: "1 month",
+  },
+  showAgeAndLangs: function (personalPlanPeter) {
+    let age = personalPlanPeter.age;
+    let languages = personalPlanPeter.skills.languages;
+    let languagesStr = languages.join(" ").toUpperCase();
+    return `Мне ${age} и я владею языками: ${languagesStr}`;
+  },
+};
 
-let usersById = groupById(users);
-console.log(usersById);
+//'Мне 29 и я владею языками: RU ENG'
+personalPlanPeter["showAgeAndLangs"] = function (plan = personalPlanPeter) {
+  let msg = `Мне ${plan.age} и я владею языками: ${plan.skills.languages
+    .join(" ")
+    .toUpperCase()}`;
 
-/*
-после вызова у нас должно получиться:
+  return msg;
+};
 
-usersById = {
-  john: {id: 'john', name: "John Smith", age: 20},
-  ann: {id: 'ann', name: "Ann Smith", age: 24},
-  pete: {id: 'pete', name: "Pete Peterson", age: 31},
+function showExperience(plan) {
+  return plan.skills.exp;
 }
-*/
-function groupById(arr = []) {
-  let res = {};
-  arr.forEach((value) => (res[value.id] = value));
-  return res;
+
+//"Язык js изучен на 20% Язык php изучен на 10%";
+function showProgrammingLangs(plan = personalPlanPeter) {
+  let mes = "";
+  let programmingLangs = plan.skills.programmingLangs;
+
+  for (let [Lang, proc] of Object.entries(programmingLangs)) {
+    mes += `Язык ${Lang} изучен на ${proc}.\n`;
+  }
+
+  return mes;
 }
+
+function showProgrammingLangs1(plan) {
+  let programmingLangs = plan.skills.programmingLangs;
+  let msg = ``;
+  for (let key in programmingLangs) {
+    msg = msg + `Язык ${key} изучен на ${programmingLangs[key]}\n`;
+  }
+
+  //msg = msg.slice(0, msg.length - 1);
+  return msg; //
+}
+
+//console.log(showProgrammingLangs(personalPlanPeter));
+
+//console.log(showExperience(personalPlanPeter));
+
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
