a45de104cb78e984d78d1b321c29d28ec654f42e
Stas <stas_ostrovsky@hotmail.com>
2025-11-17 10:36:53 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
-const baseCurrencies = ["USD", "EUR"];
-const additionalCurrencies = ["UAH", "RUB", "CNY"];
-
-function availableCurr(arr = [], missingCurr) {
-  if (arr == 0) {
-    return "Нет доступных валют";
-  }
-  let myArr = arr.filter((value) => value !== missingCurr);
-  return myArr.join("\n");
+function Calculator() {
+  this.a = 0;
+  this.b = 0;
+  this.read = function () {
+    this.a = Math.random(); //+prompt("Введите число a", "0");
+    this.b = Math.random(); //+prompt("Введите число b", "0");
+  };
+  this.sum = function () {
+    return this.a + this.b;
+  };
+  this.mul = function () {
+    return this.a * this.b;
+  };
+  return this;
 }
 
-let arr = [...baseCurrencies, ...additionalCurrencies];
-console.log(arr);
+let calculator = new Calculator();
+calculator.read();
 
-console.log(availableCurr(arr, "CNY"));
+console.log("Sum=" + calculator.sum());
+console.log("Mul=" + calculator.mul());
