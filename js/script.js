e58b86eca2d40ab265758876a31cd5f168daa303
Stas <stas_ostrovsky@hotmail.com>
2025-11-17 14:32:21 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,19 +1,7 @@
-let calculator = {
-  a: 0,
-  b: 0,
-  read() {
-    this.a = Math.random();
-    this.b = Math.random();
-  },
-  sum() {
-    return this.a + this.b;
-  },
-  mul() {
-    return this.a * this.b;
-  },
+let user = {
+  name: "John",
 };
 
-calculator.read();
+let descriptor = Object.getOwnPropertyDescriptor(user, "name");
 
-console.log(calculator.sum());
-console.log(calculator.mul());
+console.log(JSON.stringify(descriptor, null, 2));
