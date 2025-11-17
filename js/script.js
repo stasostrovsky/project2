abbd787521b32faed5f556b778faec34fb8ac8b3
Stas <stas_ostrovsky@hotmail.com>
2025-11-17 11:19:28 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,21 +1,19 @@
-function Calculator() {
-  this.a = 0;
-  this.b = 0;
-  this.read = function () {
-    this.a = Math.random(); //+prompt("Введите число a", "0");
-    this.b = Math.random(); //+prompt("Введите число b", "0");
-  };
-  this.sum = function () {
+let calculator = {
+  a: 0,
+  b: 0,
+  read() {
+    this.a = Math.random();
+    this.b = Math.random();
+  },
+  sum() {
     return this.a + this.b;
-  };
-  this.mul = function () {
+  },
+  mul() {
     return this.a * this.b;
-  };
-  return this;
-}
+  },
+};
 
-let calculator = new Calculator();
 calculator.read();
 
-console.log("Sum=" + calculator.sum());
-console.log("Mul=" + calculator.mul());
+console.log(calculator.sum());
+console.log(calculator.mul());
