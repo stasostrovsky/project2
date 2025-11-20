ca61e64706c20f824abb719e01f2ae0de5fe6657
Stas <stas_ostrovsky@hotmail.com>
2025-11-20 12:15:43 +0200

    Decorator-spy

--- js/script.js
+++ js/script.js
@@ -1,25 +1,23 @@
-function slow(x) {
-  // здесь могут быть ресурсоёмкие вычисления
-  console.log(`Called with ${x}`);
-  return Math.random();
+function decor(func) {
+  function wrapper(...args) {
+    wrapper.calls.push(args);
+    return func.apply(this, args);
+  }
+
+  wrapper.calls = [];
+
+  return wrapper;
 }
 
-function cachingDecorator(func) {
-  let cache = new Map();
-  return function (x) {
-    if (cache.has(x)) {
-      // если кеш содержит такой x,
-      return cache.get(x); // читаем из него результат
-    }
-    let result = func(x); // иначе, вызываем функцию
-    cache.set(x, result); // и кешируем (запоминаем) результат
-    return result;
-  };
+function work(a, b) {
+  console.log(a + b);
 }
 
-slow = cachingDecorator(slow);
+work = decor(work);
 
-console.log(slow(1)); // slow(1) кешируем
-console.log("Again: " + slow(1)); // возвращаем из кеша
-console.log(slow(2)); // slow(2) кешируем
-console.log("Again: " + slow(2)); // возвращаем из кеша
+work(1, 2); // 3
+work(4, 5); // 9
+
+for (let args of work.calls) {
+  console.log("call:" + args.join());
+}
