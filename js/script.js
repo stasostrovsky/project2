f2ecf722661ceee68c5651402915769a3c1a6c6b
Stas <stas_ostrovsky@hotmail.com>
2025-11-25 11:38:49 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-let obj = {
-  0: "Hello",
-  1: "world!",
-  length: 2,
-  join: Array.prototype.join,
-};
+let num = 35; //+prompt("Введите положительное целое число?", 35);
+let diff, result;
+let start = Date.now();
 
-//obj.join = Array.prototype.join;
+try {
+  result = fib(num);
+} catch (e) {
+  result = 0;
+} finally {
+  diff = Date.now() - start;
+}
 
-console.log(obj.join(",")); // Hello,world!
+console.log(result || "возникла ошибка");
+
+console.log(`Выполнение заняло ${diff}ms`);
+
+function fib(n) {
+  if (n < 0 || Math.trunc(n) != n) {
+    throw new Error("Должно быть целое неотрицательное число");
+  }
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
